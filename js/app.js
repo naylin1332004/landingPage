a57cb8664006c81f4a6fe6a3678108cc9de609a0
naylin1332004 <lin323411@gmail.com>
2025-08-10 14:20:26 +0630

    modified content&js

---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    // Start Nav Bar
+    $(window).scroll(function(){
+
+        let getscrolltop = $(this).scrollTop();
+        // console.log(getscrolltop);
+
+        if(getscrolltop >= 200){
+            $('.navbar').addClass("navmenus");
+        }else{
+            $('.navbar').removeClass("navmenus");
+        }
+    });
+
+    $('.navbuttons').click(function(){
+        $(this).toggleClass('crossxs');
+    });
+    // End Nav Bar
+
+
+});
