73375eb8999b97d679dfe95efbffd66e8c74d541
naylin1332004 <lin323411@gmail.com>
2025-08-18 20:31:57 +0630

    modified services

--- js/app.js
+++ js/app.js
@@ -17,5 +17,52 @@ $(document).ready(function(){
     });
     // End Nav Bar
 
+        //  <!-- start Property Section  -->
 
+
+        $(".propertylists").click(function(){
+            // $(this).addClass("activeitems");
+
+            $(this).addClass("activeitems").siblings().removeClass('activeitems');
+
+            const filtervalue =$(this).attr('data-filter'); 
+            // const filtervalue = $('this').data('filter');
+
+            console.log(filtervalue);
+
+            if(filtervalue === "all"){
+                $(".filters").show("slide",500);
+            }else{
+
+                //Method 1
+                // $(".filters").hide();
+
+                // $('.filters').filter('.'+filtervalue).show();
+
+
+                // Method 2
+                $('.filters').not('.'+filtervalue).hide("slide",500);
+                $('.filters').filter('.'+filtervalue).show("slide",500);
+            }
+
+        });
+        //  <!-- END Property Section  -->
+
+
+        // Start Adv Section 
+        $(window).scroll(function(){
+
+        let getscrolltop = $(this).scrollTop();
+        // console.log(getscrolltop);
+
+        if(getscrolltop >= 1200){
+            $('.adimages').addClass("fromlefts");
+            $('.advtexts').addClass("fromrights");
+        }else{
+            $('.adimages').removeClass("fromlefts");
+            $('.advtexts').removeClass("fromrights");
+        }
+    });
+
+        // End Adv Section 
 });
